aeb1989d9a5bed5cfa60c0566f7eaa184e6488c8
rajuverma-dev <rajuverma.dev@gmail.com>
2025-07-17 00:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Azure App Service!');
+    res.send('Hello Gif Creater!');
 });
 
 app.listen(port, () => {
